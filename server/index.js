19d8fbc321d34784687383ff8abbd429def18c58
Aaron Kagan <aaronkagandev@gmail.com>
2022-11-27 14:10:18 -0500

    adding upload limit

--- server/index.js
+++ server/index.js
@@ -5,7 +5,9 @@ const port = 8000;
 
 express()
   // This line will allow our server to auto-parse the body, which arrives as as a JSON string, to an object that we can use. (The same as JSON.parse())
-  .use(express.json())
+  // The upload limit is set to fix errors I was getting with file uploads
+  .use(express.json({ limit: "10mb" }))
+  .use(express.urlencoded({ extended: true, limit: "10mb" }))
 
   // Helps with CORS error.
   .use(helmet())
