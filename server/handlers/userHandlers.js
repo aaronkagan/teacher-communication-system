5ca5a5f76aba45bd1a6c83318cf3649e4e997581
Aaron Kagan <aaronkagandev@gmail.com>
2022-11-27 15:13:00 -0500

    adding user endpoints and handlers

---
+++ server/handlers/userHandlers.js
@@ -0,0 +1,138 @@
+// USER HANDLERS
+
+// Setup for connecting to mongoDb
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+const { MONGO_URI } = process.env;
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+};
+
+// Get all users
+const getUsers = async (res, req) => {
+  const client = new MongoClient(MONGO_URI, options);
+
+  try {
+    await client.connect();
+    console.log("connected");
+    const db = client.db("TaskBoard");
+    const result = await db.collection("users").find().toArray();
+    const data = result.map((user) => {
+      return {
+        userId: user.userId,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        username: user.userName,
+        email: user.email,
+        role: user.role
+      };
+    });
+
+    return res.status(200).json({ status: 200, data: data });
+  } catch (err) {
+    return res.status(500).json({ status: 500, error: err });
+  } finally {
+    await client.close();
+    console.log("disconnected");
+  }
+};
+
+// Get a single user based on userId
+const getUser = async (req, res) => {
+  const client = new MongoClient(MONGO_URI, options);
+  const userId = req.params.userId;
+
+  try {
+    await client.connect();
+    console.log("connected");
+    const db = client.db("TaskBoard");
+    const result = await db.collection("users").findOne({ userId: userId });
+    const payload = {
+      userId: result.userId,
+      firstName: result.firstName,
+      lastName: result.lastName,
+      username: result.userName,
+      email: result.email,
+      role: result.role
+    };
+    return res.status(200).json({ status: 200, data: payload });
+  } catch (err) {
+    return res.status(500).json({ status: 500, error: err });
+  } finally {
+    await client.close();
+    console.log("disconnected");
+  }
+};
+
+// Add new user
+
+const addUser = async (req, res) => {
+  const client = new MongoClient(MONGO_URI, options);
+
+  const payload = { ...req.body, password: generateHashedPassword(req.body.password) };
+
+  try {
+    await client.connect();
+    console.log("connected");
+    const db = client.db("TaskBoard");
+    const result = await db.collection("users").insertOne(payload);
+    res.json({ message: "User has been added", data: result });
+  } catch (err) {
+    console.log(err);
+  } finally {
+    await client.close();
+    console.log("disconnected");
+  }
+};
+
+// Edit a user
+
+const editUser = async (req, res) => {
+  const { firstName, lastName, email, role, userId, password } = req.body;
+
+  const client = new MongoClient(MONGO_URI, options);
+
+  const payload = {
+    firstName: firstName,
+    lastName: lastName,
+    email: email,
+    password: generateHashedPassword(password),
+    role: role
+  };
+
+  try {
+    await client.connect();
+    console.log("connected");
+    const db = client.db("TaskBoard");
+    const result = await db.collection("users").updateOne({ userId: userId }, { $set: payload });
+
+    return res.status(200).json({ status: 200, message: "User updated" });
+  } catch (err) {
+    return res.status(500).json({ status: 500, error: err });
+  } finally {
+    await client.close();
+    console.log("disconnected");
+  }
+};
+
+// Delete a user based on userId
+const deleteUser = async (req, res) => {
+  userId = req.params.userId;
+  const client = new MongoClient(MONGO_URI, options);
+
+  try {
+    await client.connect();
+    console.log("connected");
+    const db = client.db("TaskBoard");
+    const result = await db.collection("users").deleteOne({ userId: userId });
+    return res.status(200).json({ status: 200, message: "User has been deleted" });
+  } catch (err) {
+    return res.status(500).json({ status: 500, error: err });
+  } finally {
+    await client.close();
+    console.log("disconnected");
+  }
+};
+
+module.exports = { getUsers, getUser, addUser, editUser, deleteUser };
--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const port = 8000;
 
 const { modifyTaskBoard, getWeeklyTasks } = require("./handlers/taskBoardHandlers");
 const { login } = require("./handlers/loginHandlers");
+const { getUsers, getUser, addUser, editUser, deleteUser } = require("./handlers/userHandlers");
 
 express()
   // This line will allow our server to auto-parse the body, which arrives as as a JSON string, to an object that we can use. (The same as JSON.parse())
@@ -33,8 +34,20 @@ express()
   .patch("/api/tasks", modifyTaskBoard)
 
   //Login endpoints
+  // Login user based on username in body
   .post("/api/login", login)
 
+  // User endpoints
+  // Get all users
+  .get("/api/users", getUsers)
+  // Get user based on userId
+  .get("/api/user/:userId", getUser)
+  // Add a new user
+  .post("/api/user", addUser)
+  // Edit a user based on userId in body
+  .patch("/api/user", editUser)
+  // Delete a user
+  .delete("/api/user/:userId", deleteUser)
   //-------------------
 
   // Must be below all the endpoints
