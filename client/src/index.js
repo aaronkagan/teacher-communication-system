ba00030c67397430564ae70a281b58fcf7232b17
Aaron Kagan <aaronkagandev@gmail.com>
2022-11-28 15:55:01 -0500

    removing tasks context provider

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { TasksProvider } from "./contexts/TasksContext";
 import { UserProvider } from "./contexts/UserContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
   <UserProvider>
-    <TasksProvider>
-      <App />
-    </TasksProvider>
+    <App />
   </UserProvider>
   // </React.StrictMode>
 );
