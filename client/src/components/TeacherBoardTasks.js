c2d2feb0a36c6495f92915dba2da5a513fe2c5af
Aaron Kagan <aaronkagandev@gmail.com>
2022-11-27 21:13:08 -0500

    simplifying task card

--- client/src/components/TeacherBoardTasks.js
+++ client/src/components/TeacherBoardTasks.js
@@ -7,9 +7,7 @@ const TeacherBoardTask = ({ task, index }) => {
       {(provided, snapshot) => (
         <Container {...provided.draggableProps} ref={provided.innerRef} isDragging={snapshot.isDragging}>
           <TaskCard {...provided.dragHandleProps}>
-            <h3>{task.title}</h3>
             <p>{task.content}</p>
-            <h5>Due: {task.dueDate}</h5>
           </TaskCard>
         </Container>
       )}
