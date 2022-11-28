7d2e8a87a74d21ea712b354e8a15692e76557b5d
Aaron Kagan <aaronkagandev@gmail.com>
2022-11-27 20:45:04 -0500

    basic column setup

--- client/src/components/TeacherBoardColumn.js
+++ client/src/components/TeacherBoardColumn.js
@@ -1,7 +1,28 @@
+import styled from "styled-components";
+import { Droppable } from "react-beautiful-dnd";
+
 import capitalize from "../functions/capitalize";
 
 const TeacherBoardColumn = ({ columnName, columnTasks }) => {
-  return <h1>{capitalize(columnName)}</h1>;
+  return (
+    <Wrapper>
+      <h3>{capitalize(columnName)}</h3>
+
+      {/* Most of this below is the boilerplate from the beautiful react drag and drop documentation except for the mapping of the individual tasks */}
+      <Droppable droppableId={columnName}>
+        {(provided, snapshot) => (
+          <TaskList ref={provided.innerRef} {...provided.droppableProps} isDraggingOver={snapshot.isDraggingOver}>
+            {columnTasks.map((task, index) => !task.isDeleted && <Task key={task.id} task={task} index={index} />)}
+            {provided.placeholder}
+          </TaskList>
+        )}
+      </Droppable>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.div``;
+
+const TaskList = styled.div``;
 
 export default TeacherBoardColumn;
