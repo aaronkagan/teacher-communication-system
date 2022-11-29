1ad518b4495ae22cd502e8cd9577762f00bc3cf8
Aaron Kagan <aaronkagandev@gmail.com>
2022-11-28 21:20:45 -0500

    adding to teacher task modal

--- client/src/modals/TeacherBoardTaskModal.js
+++ client/src/modals/TeacherBoardTaskModal.js
@@ -1,15 +1,24 @@
 import { Dialog } from "@mui/material";
+import styled from "styled-components";
 
 const TeacherBoardTaskModal = ({ isModalOpen, setIsModalOpen, task }) => {
   return (
     <Dialog open={isModalOpen}>
-      {console.log(task)}
-      <h1>{task.message}</h1>
-      <button type="button" onClick={() => setIsModalOpen(false)}>
-        Close
-      </button>
+      <Wrapper>
+        {console.log(task)}
+        <h3>{task.title}</h3>
+        <p>{task.message}</p>
+        <h5>Due: {task.dueDate}</h5>
+        <button type="button" onClick={() => setIsModalOpen(false)}>
+          Close
+        </button>
+      </Wrapper>
     </Dialog>
   );
 };
+
+const Wrapper = styled.div`
+  padding: 20px;
+`;
 
 export default TeacherBoardTaskModal;
