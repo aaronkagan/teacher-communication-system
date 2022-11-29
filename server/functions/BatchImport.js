f16bd7233f196dc6bce615cdedc0ca4acc2f3bac
Aaron Kagan <aaronkagandev@gmail.com>
2022-11-28 21:53:36 -0500

    adjusting default import

--- server/functions/BatchImport.js
+++ server/functions/BatchImport.js
@@ -10,17 +10,18 @@ const options = {
 const initialData = {
   weekId: "1",
   tasks: {
-    "task-1": { id: "task-1", title: "Do this", dueDate: "Dec 5, 2022", message: "Take out the garbage" },
-    "task-2": { id: "task-2", title: "Do that", dueDate: "Dec 5, 2022", message: "Buy food" },
-    "task-3": { id: "task-3", title: "Do the other", dueDate: "Dec 5, 2022", message: "Mow the lawn" },
-    "task-4": { id: "task-4", title: "Or the best", dueDate: "Dec 5, 2022", message: "Wash the car" },
-    "task-5": { id: "task-5", title: "Thing", dueDate: "Dec 5, 2022", message: "Do homework" }
+    // "task-1": { id: "task-1", title: "Do this", dueDate: "Dec 5, 2022", message: "Take out the garbage" },
+    // "task-2": { id: "task-2", title: "Do that", dueDate: "Dec 5, 2022", message: "Buy food" },
+    // "task-3": { id: "task-3", title: "Do the other", dueDate: "Dec 5, 2022", message: "Mow the lawn" },
+    // "task-4": { id: "task-4", title: "Or the best", dueDate: "Dec 5, 2022", message: "Wash the car" },
+    // "task-5": { id: "task-5", title: "Thing", dueDate: "Dec 5, 2022", message: "Do homework" }
   },
   columns: {
     sunday: {
       id: "sunday",
       title: "Sunday",
-      taskIds: ["task-1", "task-2", "task-3", "task-4", "task-5"]
+      // taskIds: ["task-1", "task-2", "task-3", "task-4", "task-5"]
+      taskIds: []
     },
     monday: {
       id: "monday",
