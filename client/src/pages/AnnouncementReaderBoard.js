daf0be0d2b62bf6e8966bf9bc6af0d6a6ccbd68b
Aaron Kagan <aaronkagandev@gmail.com>
2022-11-30 12:19:16 -0500

    basic reader announcement setup

---
+++ client/src/pages/AnnouncementReaderBoard.js
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+import Announcement from "../components/Announcement";
+
+const AnnouncementReaderBoard = () => {
+  const [announcements, setAnnouncements] = useState();
+
+  // useEffect(() => {
+  //   fetch("/api/announcements")
+  //     .then((res) => res.json())
+  //     .then((data) => setAnnouncements(data.data))
+  //     .catch((err) => console.log(err));
+  // }, []);
+
+  return (
+    <Wrapper>
+      <h1>Announcement Board</h1>
+      {announcements &&
+        announcements.map((announcement) => {
+          return <Announcement key={announcement.announcementId} announcement={announcement} />;
+        })}
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div``;
--- client/src/pages/TeacherAnnouncements.js
+++ client/src/pages/TeacherAnnouncements.js
@@ -1,13 +1,28 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 import Announcement from "../components/Announcement";
 import CreateAnnouncementModal from "../modals/CreateAnnouncementModal";
+import getUserId from "../functions/getUserId";
 
 const TeacherAnnouncements = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [myAnnouncements, setMyAnnouncements] = useState();
+  const userId = getUserId();
+
+  // useEffect(() => {
+  //   fetch(`api/announcements/${userId}`)
+  //     .then((res) => res.json())
+  //     .then((data) => setMyAnnouncements(data.data))
+  //     .catch((err) => console.log(err));
+  // }, []);
   return (
     <Wrapper>
-      <button onClick={() => setIsModalOpen(true)}>Open</button>
+      <h1>My Announcements</h1>
+      {myAnnouncements &&
+        myAnnouncements.map((announcement) => {
+          return <Announcement key={announcement.announcementId} announcement={announcement} />;
+        })}
+      <button onClick={() => setIsModalOpen(true)}>Create Announcement</button>
       <CreateAnnouncementModal isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} />
     </Wrapper>
   );
