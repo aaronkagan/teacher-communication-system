d7924476099219394c1499f61f6a0e1658dec17c
Aaron Kagan <aaronkagandev@gmail.com>
2022-12-04 00:10:36 -0500

    dashboard cards

---
+++ client/src/data/DashboardCardData.js
@@ -0,0 +1,34 @@
+const DashboardData = {
+  TeacherBoardCard: {
+    image: require("../style/assets/images/teacher-board-card-image.png"),
+    text: "Teacher Task Board",
+    linkTo: "/board/teacher",
+    id: 1
+  },
+  StudentBoardCard: {
+    image: require("../style/assets/images/student-board-card-image.png"),
+    text: "Student Task Board",
+    linkTo: "/board/student",
+    id: 2
+  },
+  TeacherAnnouncements: {
+    image: require("../style/assets/images/teacher-announcements-image.png"),
+    text: "Teacher Announcements",
+    linkTo: "/announcements/teacher",
+    id: 3
+  },
+  ReaderAnnouncements: {
+    image: require("../style/assets/images/reader-image.png"),
+    text: "Reader Announcement",
+    linkTo: "/announcements/reader",
+    id: 4
+  },
+  UserAdmin: {
+    image: require("../style/assets/images/user-admin-image.png"),
+    text: "User Administration",
+    linkTo: "/users",
+    id: 5
+  }
+};
+
+export default DashboardData;
--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,7 +1,8 @@
 import styled from "styled-components";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { UserContext } from "../contexts/UserContext";
-
+import DashboardCard from "../components/dashboard-cards/DashboardCard";
+import DashboardData from "../data/DashboardCardData";
 const DashBoard = () => {
   const { userState } = useContext(UserContext);
   return (
@@ -10,13 +11,22 @@ const DashBoard = () => {
         <h1>Welcome {userState.firstName}</h1>
         <h3>Role: {userState.role}</h3>
       </Content>
+      <Cards>
+        {Object.values(DashboardData).map((card) => {
+          return <DashboardCard key={card.id} image={card.image} text={card.text} linkTo={card.linkTo} />;
+        })}
+      </Cards>
     </Wrapper>
   );
 };
 
 const Wrapper = styled.div`
   text-align: center;
-  margin-top: 20vh;
+  padding-top: 10vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 150px;
 `;
 
 const Content = styled.div`
@@ -26,6 +36,12 @@ const Content = styled.div`
   h3 {
     font-size: 20px;
   }
+`;
+
+const Cards = styled.div`
+  display: grid;
+  gap: 30px;
+  grid-template-columns: repeat(3, 1fr);
 `;
 
 export default DashBoard;
