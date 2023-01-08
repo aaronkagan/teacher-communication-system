baed6fa88a23b42d510286b51b3525ae05d760d3
Aaron Kagan <aaronkagandev@gmail.com>
2023-01-08 18:40:04 -0500

    updating comment

--- client/src/functions/getUserId.js
+++ client/src/functions/getUserId.js
@@ -1,4 +1,4 @@
-// Helper function to get the current logged in user ID (currently it's from local storage but will change when i implement a more secure authentication and authorization system)
+// Helper function to get the current logged in user ID (currently it's from local storage but will change when i implement a more secure authentication and authorization system) (Alternatively i could be getting this information from the userContext. No specific reason as to why i'm doing it this way)
 
 const getUserId = () => {
   return window.localStorage.getItem('userId');
