6633a48c17ca6da1fadf72a89a6655ee4e0c86d6
Aaron Kagan <aaronkagandev@gmail.com>
2023-01-08 18:37:08 -0500

    commenting random sticky color function

--- client/src/functions/getRandomStickyColor.js
+++ client/src/functions/getRandomStickyColor.js
@@ -1,5 +1,6 @@
-const stickyColors = ["yellow", "lightGreen", "pink"];
+// Helper function used to get a random sticky color for the student board tasks from the supplied colors
 
+const stickyColors = ['yellow', 'lightGreen', 'pink'];
 const getRandomStickyColor = () => {
   return stickyColors[Math.floor(Math.random() * stickyColors.length)];
 };
