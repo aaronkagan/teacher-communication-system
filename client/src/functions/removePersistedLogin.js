5cfdc96d091914f3fe5041fd2731d49f7578d684
Aaron Kagan <aaronkagandev@gmail.com>
2023-01-08 18:49:20 -0500

    commenting helper function remove login

--- client/src/functions/removePersistedLogin.js
+++ client/src/functions/removePersistedLogin.js
@@ -1,7 +1,10 @@
+// Helper function used to remove the user's persisted login information/
+// Using local storage for now but will modify helper function to use more secure method when a more robust authentication and authorization system is implemented
+
 const removePersistedLogin = () => {
-  window.localStorage.removeItem("isLoggedIn");
-  window.localStorage.removeItem("role");
-  window.localStorage.removeItem("userId");
+  window.localStorage.removeItem('isLoggedIn');
+  window.localStorage.removeItem('role');
+  window.localStorage.removeItem('userId');
 };
 
 export default removePersistedLogin;
