3f07222e3099f16229772f28e78125d147a2d5fe
Aaron Kagan <aaronkagandev@gmail.com>
2023-01-08 18:40:30 -0500

    commenting get user role helper function

--- client/src/functions/getUserRole.js
+++ client/src/functions/getUserRole.js
@@ -1,5 +1,7 @@
+// Helper function to get the current logged in user ROLE (currently it's from local storage but will change when i implement a more secure authentication and authorization system) (Alternatively i could be getting this information from the userContext. No specific reason as to why i'm doing it this way)
+
 const getUserRole = () => {
-  return window.localStorage.getItem("role");
+  return window.localStorage.getItem('role');
 };
 
 export default getUserRole;
