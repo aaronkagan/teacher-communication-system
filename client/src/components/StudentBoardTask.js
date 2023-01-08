085fb48716a8b6c034690dbc38cee4075a6f432a
Aaron Kagan <aaronkagandev@gmail.com>
2023-01-08 18:24:15 -0500

    adding comments removing conditional rendering

--- client/src/components/StudentBoardTask.js
+++ client/src/components/StudentBoardTask.js
@@ -1,19 +1,30 @@
-import { useState } from "react";
-import StickyNote from "./StickyNote";
-import getRandomStickyColor from "../functions/getRandomStickyColor";
-import getRandomStickyDirection from "../functions/getRandomStickyDirection";
-import StudentBoardTaskModal from "../modals/StudentBoardTaskModal";
+import { useState } from 'react';
+import StickyNote from './StickyNote';
+import getRandomStickyColor from '../functions/getRandomStickyColor';
+import getRandomStickyDirection from '../functions/getRandomStickyDirection';
+import StudentBoardTaskModal from '../modals/StudentBoardTaskModal';
 
 const StudentBoardTask = ({ task, boardState, setBoardState }) => {
+  // This is used to open and close the modal that students use to see details add comments to the student board task (modal from MUI)
   const [isModalOpen, setIsModalOpen] = useState(false);
   return (
     <>
-      {task && (
-        <div onClick={() => setIsModalOpen(true)}>
-          <StickyNote task={task} stickyColor={getRandomStickyColor()} stickyDirection={getRandomStickyDirection()} />
-          <StudentBoardTaskModal stickyColor={getRandomStickyColor()} task={task} isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} boardState={boardState} setBoardState={setBoardState} />
-        </div>
-      )}
+      {/* Opening the modal to see details and and add new comments to the student task*/}
+      <div onClick={() => setIsModalOpen(true)}>
+        <StickyNote
+          task={task}
+          stickyColor={getRandomStickyColor()}
+          stickyDirection={getRandomStickyDirection()}
+        />
+        <StudentBoardTaskModal
+          stickyColor={getRandomStickyColor()}
+          task={task}
+          isModalOpen={isModalOpen}
+          setIsModalOpen={setIsModalOpen}
+          boardState={boardState}
+          setBoardState={setBoardState}
+        />
+      </div>
     </>
   );
 };
