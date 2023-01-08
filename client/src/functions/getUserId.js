e6ea3d2871a03639b57e0b373bedc53db7fbf3bc
Aaron Kagan <aaronkagandev@gmail.com>
2023-01-08 18:39:11 -0500

    commenting get user id helper function

--- client/src/functions/getUserId.js
+++ client/src/functions/getUserId.js
@@ -1,5 +1,7 @@
+// Helper function to get the current logged in user ID (currently it's from local storage but will change when i implement a more secure authentication and authorization system)
+
 const getUserId = () => {
-  return window.localStorage.getItem("userId");
+  return window.localStorage.getItem('userId');
 };
 
 export default getUserId;
