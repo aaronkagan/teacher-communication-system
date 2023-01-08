9f388c42e980875e85be4f4ef0d9ad3dd6fbed08
Aaron Kagan <aaronkagandev@gmail.com>
2023-01-08 18:42:27 -0500

    commenting user logged in helper function

--- client/src/functions/isUserLoggedIn.js
+++ client/src/functions/isUserLoggedIn.js
@@ -1,5 +1,7 @@
+// Helper function to check if a user is logged in based on the login state stored in local storage. (Using local storage for now but will modify this when i implement a more secure login system)
+
 const isUserLoggedIn = () => {
-  return window.localStorage.getItem("isLoggedIn") === "true" && window.localStorage.getItem("userId") !== null;
+  return window.localStorage.getItem('isLoggedIn') === 'true' && window.localStorage.getItem('userId') !== null;
 };
 
 export default isUserLoggedIn;
