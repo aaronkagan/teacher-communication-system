a9b3ae6809dfc59afee14a616d8e589c194f142b
Aaron Kagan <aaronkagandev@gmail.com>
2023-01-08 18:52:24 -0500

    commenting user full name hook

--- client/src/hooks/useUserFullName.js
+++ client/src/hooks/useUserFullName.js
@@ -1,5 +1,7 @@
-import { useContext } from "react";
-import { UserContext } from "../contexts/UserContext";
+import { useContext } from 'react';
+import { UserContext } from '../contexts/UserContext';
+
+// Used to retrieve the users full name from the user context based on the user's first and last name
 
 const useUserFullName = () => {
   const { userState } = useContext(UserContext);
