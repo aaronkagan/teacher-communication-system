8f5faa47198f8c96ef8431cd1b3f6190624e5a4b
Aaron Kagan <aaronkagandev@gmail.com>
2023-01-08 18:11:26 -0500

    commenting logout function

--- client/src/functions/handleLogout.js
+++ client/src/functions/handleLogout.js
@@ -1,7 +1,11 @@
-import removePersistedLogin from "./removePersistedLogin";
+import removePersistedLogin from './removePersistedLogin';
+
+// This component handles the user logout
 
 const handleLogout = (setUserState) => {
+  // Setting the userState and resetting the userState to and empty object on logout
   setUserState({});
+  // Removing the logged in user's credentials based on the current method being used in the removePersistedLogin function
   removePersistedLogin();
 };
 
